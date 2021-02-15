5ad19f06bb3e65796ddef3a4d16bab11f4d19952
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2021-02-15 22:31:39 +0100

    Add files via upload

---
+++ buric-NiOP 4.d klase i objekti3/KlasaX.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buric_NiOP_4.d_klase_i_objekti3
+{
+    class KlasaX
+    {
+       public int[] arr = new int[100];
+        public int index;
+    }
+}
---
+++ buric-NiOP 4.d klase i objekti3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buric_NiOP_4.d_klase_i_objekti3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaX x = new KlasaX();
+            for (int i = 0; i < 100; i++) 
+            x.arr[i] = i;
+
+            x.index = 50;
+            Console.WriteLine(x.arr[x.index]);
+
+        }
+    }
+}
